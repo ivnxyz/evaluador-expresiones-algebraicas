033edfb65f1b1b500bc2f03b5f220c2796af4fb6
Iván Martínez <ivanmartinez33@outlook.com>
2020-12-23 13:33:04 -0600

    Se agregó la estructura Stack

---
+++ Stack.py
@@ -0,0 +1,66 @@
+# Clase Nodo para el Stack
+class Node:
+
+  # data es el dato a guardar y next es el siguiente nodo
+  def __init__(self, data, next = None):
+    self.data = data
+    self.next = next
+
+# Estructura para el Stack
+class Stack:
+  head:Node = None
+
+  # 'empuja' un nuevo nodo
+  def push(self, data):
+    # Crear nodo
+    new_node = Node(data)
+
+    # Empujar nodo
+    if self.is_empty():
+      self.head = new_node
+    else:
+      # Iterar hasta el último elemento
+      temp = self.head
+
+      while temp.next != None:
+        temp = temp.next
+      
+      # Agregar nuevo nodo
+      temp.next = new_node
+  
+  # Elimina el último nodo
+  def pop(self) -> Node:
+    # Verificar si hay elementos
+    if self.is_empty():
+      raise Exception("No hay elementos en el Stack")
+
+    # Iterar hasta el penúltimo nodo
+    last = self.head
+    current = self.head
+
+    while current.next != None:
+      last = current
+      current = current.next
+
+    # Eliminar último elemento
+    if last.next == None:
+      self.head = None
+    else:
+      last.next = None
+
+    return current
+  
+  # Verifica si el stack está vacío
+  def is_empty(self) -> bool:
+    return self.head == None
+  
+  # Sobreescribe la función str para imprimir un stack
+  def __str__(self):
+    node = self.head
+    string_representation = ''
+
+    while node != None:
+      string_representation = '[ {} ]\n'.format(node.data) + string_representation
+      node = node.next
+    
+    return string_representation
---
+++ prefix.py
@@ -0,0 +1,3 @@
+# Traduce una expresión algebráica a prefijo
+def infix_to_prefix(algebraic_expression: str):
+  pass
