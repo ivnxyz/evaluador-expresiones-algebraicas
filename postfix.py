3901f6304fe71a0bdd5a364ab97e49b85a8a0df1
Hiram <hirampaulinarista@gmail.com>
2020-12-26 23:19:39 -0600

    Se modificó y terminó el archivo de posfijo, pero no  se si funciona al cien; falta revisar

--- postfix.py
+++ postfix.py
@@ -1,2 +1,23 @@
+from Stack import Node, Stack
+from helper import is_number, get_operator_weight
+from characters import OPERATORS
 def infix_to_postfix(algebraic_expression: str):
-  pass
+  result=''
+  stacksito=Stack()
+  for char in algebraic_expression:
+    if char not in OPERATORS:
+      result+=char
+    elif char=='(':
+      stacksito.push('(')
+    elif char==')':
+      while stacksito.is_empty() and stacksito.last() is not '(':
+        result+=stacksito.pop()
+      stacksito.pop()
+    else:
+      while stacksito.is_empty() and stacksito.last()!='(' and get_operator_weight(char)<=get_operator_weight(stacksito.last()):
+        result+=stacksito.pop()
+      stacksito.append(char)
+  while stacksito:
+    result+=stacksito.pop()
+  return result
+
