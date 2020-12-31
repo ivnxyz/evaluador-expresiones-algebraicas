5d4749b0eacc3e56ce21b5325da38679240bdc00
Iván Martínez <ivanmartinez33@outlook.com>
2020-12-30 23:26:49 -0600

    Se agregó algoritmo para traducir de infijo a prefijo

--- prefix.py
+++ prefix.py
@@ -1,12 +1,14 @@
 # Importar dependencias
-from postfix import infix_to_postfix
+from Stack import Stack
+from characters import OPERATORS
+from helper import get_operator_weight
 
 # Traduce una expresión de infijo a prefijo
 def infix_to_prefix(infix_expression: str):
   # Invertir la expresión
   reversed_expression = []
 
-  for character in infix_expression[::-1]:
+  for character in infix_expression[::-1].split():
     # Reemplazar paréntesis
     if character == '(':
       reversed_expression.append(')')
@@ -15,5 +17,39 @@ def infix_to_prefix(infix_expression: str):
     else:
       reversed_expression.append(character)
   
-  posfix_expression = infix_to_postfix(''.join(reversed_expression))
-  return posfix_expression[::-1]
+  # Crear Stack para la expresión
+  expression_stack = Stack()
+  expression_stack.push('(')
+
+  # Expresión
+  result_expression_list = []
+
+  # Separar la expresión
+  characters = reversed_expression + [')']
+  print(characters)
+
+  # Iterar por los caracteres
+  for character in characters:
+    # Verificar el tipo de caracter
+    if character == '(':
+      expression_stack.push(character)
+    elif character in OPERATORS:
+      # Obtener valor en la jerarquía del operador
+      operator_value = get_operator_weight(character)
+
+      # Sacar elementos del Stack
+      while not expression_stack.is_empty() and get_operator_weight(expression_stack.last().data) >= operator_value:
+        result_expression_list.append(expression_stack.pop().data)
+      
+      # Añadir operador al Stack
+      expression_stack.push(character)
+    elif character == ')':
+      while expression_stack.last().data != '(':
+        result_expression_list.append(expression_stack.pop().data)
+      
+      # Eliminar paréntesis
+      expression_stack.pop()
+    else:
+      result_expression_list.append(character)
+
+  return ' '.join(result_expression_list)[::-1]
